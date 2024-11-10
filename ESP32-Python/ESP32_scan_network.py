8c4eb441a841cb04a4ee24b1cca2175f4fc8ff70
hazemone <118279810+hazemone@users.noreply.github.com>
2024-11-10 20:58:18 +0000

    ESP32_scan_network.py

---
+++ ESP32-Python/ESP32_scan_network.py
@@ -0,0 +1,14 @@
+# Wi-Fi scanner using network library 
+import network 
+
+print("Scanning for Wi-Fi networks, please wait...", "") 
+
+sta_if = network.WLAN(network.STA_IF) 
+sta_if.active(True) 
+authmodes = ['Open', 'WEP', 'WPA-PSK' 'WPA2-PSK4', 'WPA/WPA2-PSK'] 
+for (ssid, bssid, channel, RSSI, authmode, hidden) in sta_if.scan(): 
+print("* {:s}".format(ssid)) 
+print("   - Channel: {}".format(channel)) 
+print("   - RSSI: {}".format(RSSI)) 
+print("   - BSSID:{:02x}:{:02x}:{:02x}:{:02x}:{:02x}:{:02x}".format(*bssid)) 
+print() 
