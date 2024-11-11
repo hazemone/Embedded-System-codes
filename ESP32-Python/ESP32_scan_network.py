ec0eaa2b8a51c6c944f2040d0cc08ad15afbc6b1
hazemone <118279810+hazemone@users.noreply.github.com>
2024-11-11 09:52:27 +0000

    Update ESP32_scan_network.py

--- ESP32-Python/ESP32_scan_network.py
+++ ESP32-Python/ESP32_scan_network.py
@@ -7,8 +7,8 @@ sta_if = network.WLAN(network.STA_IF)
 sta_if.active(True) 
 authmodes = ['Open', 'WEP', 'WPA-PSK' 'WPA2-PSK4', 'WPA/WPA2-PSK'] 
 for (ssid, bssid, channel, RSSI, authmode, hidden) in sta_if.scan(): 
-print("* {:s}".format(ssid)) 
-print("   - Channel: {}".format(channel)) 
-print("   - RSSI: {}".format(RSSI)) 
-print("   - BSSID:{:02x}:{:02x}:{:02x}:{:02x}:{:02x}:{:02x}".format(*bssid)) 
-print() 
+  print("* {:s}".format(ssid)) 
+  print("   - Channel: {}".format(channel)) 
+  print("   - RSSI: {}".format(RSSI)) 
+  print("   - BSSID:{:02x}:{:02x}:{:02x}:{:02x}:{:02x}:{:02x}".format(*bssid)) 
+  print() 
